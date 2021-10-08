9b5e7c499535e166c0a29d649b32a9eeaaad60d5
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-08 12:26:43 +0200

    adding method in repository layer to get legends by id

--- football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/repository/LegendRepository.java
+++ football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/repository/LegendRepository.java
@@ -6,13 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LegendRepository {
+    //mocking the database
     private List<Legend> legendDatabase = new ArrayList<Legend>(List.of(
             new Legend(1, "Johan Cruyff", "Dutch", 1947),
-            new Legend(1, "Diego Armando Maradona", "Argentinian", 1960),
-            new Legend(1, "Ronaldinho Gáucho", "Brazilian", 1980)
+            new Legend(2, "Diego Armando Maradona", "Argentinian", 1960),
+            new Legend(3, "Ronaldinho Gáucho", "Brazilian", 1980)
     ));
 
     public List<Legend> findAllLegends() {
         return legendDatabase;
+    }
+
+    public Legend findLegendById(int id) {
+        Legend selectedLegendById = legendDatabase.stream()
+                .filter((legend) -> legend.getId() == id)
+                .findFirst()
+                .orElse(null);
+        return selectedLegendById;
     }
 }
