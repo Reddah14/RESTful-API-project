c4b3b66126e23379ba551e11c367e9929fc1cca0
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-08 11:45:12 +0200

    initial commit

---
+++ football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/FootballLegendsApplication.java
@@ -0,0 +1,13 @@
+package com.footballersAPI.footballlegends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballLegendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballLegendsApplication.class, args);
+	}
+
+}
---
+++ football-legends/football-legends/src/test/java/com/footballersAPI/footballlegends/FootballLegendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballersAPI.footballlegends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballLegendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
