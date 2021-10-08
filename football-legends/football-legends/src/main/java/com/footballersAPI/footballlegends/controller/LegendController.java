8c222ddb02a0a1c00822f273a3a281034f374e24
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-08 12:27:39 +0200

    adding route in controller to get legend by id

--- football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/controller/LegendController.java
+++ football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/controller/LegendController.java
@@ -5,6 +5,7 @@ import com.footballersAPI.footballlegends.repository.LegendRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,14 +14,23 @@ import java.util.List;
 @RestController
 public class LegendController {
 
-        private final LegendRepository repository = new LegendRepository();
+    private final LegendRepository repository = new LegendRepository();
 
-        // index route to find all legends
+        // INDEX ROUTE - to find all legends
         // GET /legends
     @GetMapping("/legends")
-    public ResponseEntity<List<Legend>> getAllLegends() {
+    public ResponseEntity<List<Legend>> indexLegends() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(repository.findAllLegends());
+    }
+
+        // SHOW ROUTE - find legend by id
+        // GET /legends/{id}
+    @GetMapping("/legends/{id}")
+    public ResponseEntity<Legend> showLegend(@PathVariable int id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(repository.findLegendById(id));
     }
 }
