c699767d4092551ff84d66db70219df093051959
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-10 21:07:39 +0200

    adding methods in repository for update and remove

--- football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/repository/LegendRepository.java
+++ football-legends/football-legends/src/main/java/com/footballersAPI/footballlegends/repository/LegendRepository.java
@@ -6,17 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LegendRepository {
-    //mocking the database
+    // mocking the database
     private List<Legend> legendDatabase = new ArrayList<Legend>(List.of(
             new Legend(1, "Johan Cruyff", "Dutch", 1947),
             new Legend(2, "Diego Armando Maradona", "Argentinian", 1960),
             new Legend(3, "Ronaldinho Gáucho", "Brazilian", 1980)
     ));
 
+    // method for index route
     public List<Legend> findAllLegends() {
         return legendDatabase;
     }
 
+    // method for show route
     public Legend findLegendById(int id) {
         Legend selectedLegendById = legendDatabase.stream()
                 .filter((legend) -> legend.getId() == id)
@@ -25,7 +27,33 @@ public class LegendRepository {
         return selectedLegendById;
     }
 
-    public void addLegend(Legend newLegend) {
+    // method for create route
+    public Legend addLegend(Legend newLegend) {
         legendDatabase.add(newLegend);
+        return newLegend;
+    }
+
+    // method for update route
+    public Legend editLegendById(int id, Legend updatedLegend) {
+        //finding legend to be updated
+        Legend currentLegend = findLegendById(id);
+        //finding index of legend to be updated
+        int index = legendDatabase.indexOf(currentLegend);
+        //setting in dB updatedLegend at selected index
+        legendDatabase.set(index, updatedLegend);
+        return updatedLegend;
+    }
+
+    // method for remove route
+    public Boolean removeLegendById(int id) {
+        Boolean result = false;
+        Legend legendToBeRemoved = findLegendById(id);
+        try {
+            legendDatabase.remove(legendToBeRemoved);
+            result = true;
+        } catch (Exception e) {
+            result = false;
+        }
+        return result;
     }
 }
